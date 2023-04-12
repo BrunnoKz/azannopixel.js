25da1d066e2d94d262d6ff39e8b6e68069e156ee
BrunnoKz <74139100+BrunnoKz@users.noreply.github.com>
2023-04-13 00:04:47 +0200

    Update pixel.js

--- pixel.js
+++ pixel.js
@@ -18,18 +18,3 @@
             fbq('init', '637792418174114');
             fbq('track', 'PageView');
     
-!function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
-},s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
-a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
-twq('config','oel63');
-
-
-
-
- async src="https://www.googletagmanager.com/gtag/js?id=AW-11121563095"
-
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'AW-11121563095');
